00db0debdba642ad339d170d3efc5a7d1abe7d67
ccalzada <ccalzada@emeal.nttdata.com>
2022-07-07 11:16:49 +0200

    Fin sección 7

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+  constructor( private settingsSvc: SettingsService) {}
+
+  ngOnInit(): void {
+    this.settingsSvc.checkCurrentTheme()
+  }
+  
+  changeTheme(theme: string) {
+    this.settingsSvc.changeTheme(theme)
+  }
+
+
+}
